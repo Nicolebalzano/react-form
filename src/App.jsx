e16944bd9e483f0649dc007cf7669feb70c0790f
Nicolebalzano <nicole.balzano03@gmail.com>
2025-06-06 15:58:55 +0200

    feat:Milestone 2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
 
+import { useState } from "react"
 import blogPosts from "./blogPosts.js"
 
 function App() {
+  const [newArticle, setNewArticle] = useState("");
+const[newArticleName, setNewArticleName] = useState(blogPosts)
 
+const gestisciSubmit = (event) => {
+  event.preventDefault();
+  if(newArticle.trim() !== ""){
+    setNewArticleName([...newArticleName, { id: blogPosts.length + 1, titolo: newArticle }]);
+  setNewArticle("");
+  }
+  
+};
 
   return (
     <>
     <div className="container my">
-      <form action="">
-        <label htmlFor="articleName">Inserisci il nome del nuovo articolo<input type="text" id="articleName"/></label>
-        </form> <hr />
+      <form onSubmit={gestisciSubmit}>
+        <label htmlFor="articleName">
+          Inserisci il titolo del nuovo articolo
+          <input 
+          aria-label="Nuovo titolo"
+          value={newArticle}
+          onChange={(event)  => setNewArticle(event.target.value)}
+          autoComplete="off"
+          type="text"
+           id="articleName"/>
+          </label>
+          <button type="submit">Aggiungi</button>
+        </form> 
+        <hr />
     <ul>
-      {(blogPosts.map((curPost) => {
-       return <li key={curPost.id}><h2>{curPost.titolo}</h2></li>
+      {(newArticleName.map((curPost, index) => {
+       return <li key={index}><h2>{curPost.titolo}</h2></li>
       }))}
       </ul>
       </div>
