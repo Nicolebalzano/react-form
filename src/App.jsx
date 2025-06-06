cd3804d568d66720b7b75458cc4bfca8f443c6d4
Nicolebalzano <nicole.balzano03@gmail.com>
2025-06-06 15:17:00 +0200

    feat:form added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ function App() {
   return (
     <>
     <div className="container my">
+      <form action="">
+        <label htmlFor="articleName">Inserisci il nome del nuovo articolo<input type="text" id="articleName"/></label>
+        </form> <hr />
     <ul>
       {(blogPosts.map((curPost) => {
        return <li key={curPost.id}><h2>{curPost.titolo}</h2></li>
